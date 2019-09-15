d019e70f3895b4004d3c62fe5967ae56fb31cee8
forax <forax@univ-mlv.fr>
2019-09-15 23:33:35 +0200

    simplify (supplier not needed)

--- src/fr/umlv/smalljs/main/Main.java
+++ src/fr/umlv/smalljs/main/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import fr.umlv.smalljs.astinterp.main.ASTInterpreterMain;
 
@@ -24,8 +23,8 @@ public class Main {
 			return args -> find(args).or(() -> finder.find(args));
 		}
 		
-		static Finder<String> of(String interpreterName, Supplier<Runner> interpreter) {
-			return name -> Optional.of(name).filter(interpreterName::equals).map(__ -> interpreter.get());
+		static Finder<String> of(String interpreterName, Runner runner) {
+			return name -> Optional.of(name).filter(interpreterName::equals).map(__ -> runner);
 		}
 	}
 	
@@ -34,14 +33,13 @@ public class Main {
 	}
 	
   public static void main(String[] args) throws IOException {
-		var finder = Finder.of("ast", () -> ASTInterpreterMain::main)
-		    //.or(Finder.of("stack", () -> StackInterpreterMain::main)
-				//.or(Finder.of("jvm", () -> JVMInterpreterMain::main)
+		var finder = Finder.of("ast", ASTInterpreterMain::main)
+		    //.or(Finder.of("stack", StackInterpreterMain::main)
+				//.or(Finder.of("jvm", JVMInterpreterMain::main)
 				.invmap((String[] _args) -> Optional.of(_args).filter(__args -> __args.length >= 1).map(__args -> __args[0]))
 		    .or(_args -> Optional.of(__ -> {
 		  	  System.out.println("java fr.umlv.minijs.main.Main ast|stack|jvm [script.js]");
-		  	  System.out.println();
-		  	  System.out.println("  invalid arguments " + String.join(", ", _args));
+		  	  System.out.println("  invalid arguments \"" + String.join(" ", _args) + "\"");
 		    }));
 		
 		finder.find(args).orElseThrow().run(shift(args));
