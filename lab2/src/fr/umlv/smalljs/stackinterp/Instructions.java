3a7c50f89e1f31ae75b6bdcd3f926109008ba968
forax <forax@univ-mlv.fr>
2019-10-03 22:55:34 +0200

    add SWAP

--- lab2/src/fr/umlv/smalljs/stackinterp/Instructions.java
+++ lab2/src/fr/umlv/smalljs/stackinterp/Instructions.java
@@ -8,25 +8,27 @@ public interface Instructions {
 	int STORE = 5; // STORE slot_index
 	int DUP = 6;
 	int POP = 7;
-	int FUNCALL = 8; // FUNCALL argument_count
-	int RET = 9;
-	int GOTO = 10; // GOTO instr_index
-	int JUMP_IF_FALSE = 11; // JUMP_IF_FALSE instr_index
-	int NEW = 12; // NEW dictionary_index (JSObject object)
-	int GET = 13; // GET dictionary_index (String field_name)
-	int PUT = 14; // PUT dictionary_index (String field_name)
+	int SWAP = 8;
+	int FUNCALL = 9; // FUNCALL argument_count
+	int RET = 10;
+	int GOTO = 11; // GOTO instr_index
+	int JUMP_IF_FALSE = 12; // JUMP_IF_FALSE instr_index
+	int NEW = 13; // NEW dictionary_index (JSObject object)
+	int GET = 14; // GET dictionary_index (String field_name)
+	int PUT = 15; // PUT dictionary_index (String field_name)
 
 	int PRINT = 20;
 
 	static void dump(int[] instrs, Dictionary dict) {
-		var strings = new String[] { null, "CONST", "LOOKUP", "REGISTER", "LOAD", "STORE", "DUP", "POP", "FUNCALL", "RET",
-				"GOTO", "JUMP_IF_FALSE", "NEW", "GET", "PUT", null, null, null, null, null, "PRINT" };
+		var strings = new String[] { null, "CONST", "LOOKUP", "REGISTER", "LOAD", "STORE", "DUP", "POP", "SWAP",
+				"FUNCALL", "RET", "GOTO", "JUMP_IF_FALSE", "NEW", "GET", "PUT", null, null, null, null, "PRINT" };
 		for (var pc = 0; pc < instrs.length;) {
 			System.err.print(pc + " ");
 			var instr = instrs[pc++];
 			switch (instr) {
 			case DUP: // no-arg instr
 			case POP:
+			case SWAP:
 			case RET:
 			case PRINT:
 				System.err.println(strings[instr]);
