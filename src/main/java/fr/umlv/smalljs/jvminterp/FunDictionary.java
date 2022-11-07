e3b31b216264f63989f090000eeb897995e3cfa5
forax <forax@univ-mlv.fr>
2022-11-07 09:48:39 +0100

    fix typo dictionnary -> dictionary

--- src/main/java/fr/umlv/smalljs/jvminterp/FunDictionary.java
+++ src/main/java/fr/umlv/smalljs/jvminterp/FunDictionary.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import fr.umlv.smalljs.ast.Expr.Fun;
 
 class FunDictionary {
-  private final ArrayList<Fun> dictionnary = new ArrayList<>();
+  private final ArrayList<Fun> dictionary = new ArrayList<>();
   
   int register(Fun fun) {
-    var id = dictionnary.size();
-    dictionnary.add(fun);
+    var id = dictionary.size();
+    dictionary.add(fun);
     return id;
   }
   
   Fun lookupAndClear(int id) {
-    var fun = dictionnary.get(id);
-    dictionnary.set(id, null);     // Fun will be garbage collected
+    var fun = dictionary.get(id);
+    dictionary.set(id, null);     // Fun will be garbage collected
     return fun;
   }
 }
